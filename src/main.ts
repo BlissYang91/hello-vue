45e19ae8bb8719802211a567554b49ef0759a56c
BlissYang <2812420513@qq.com>
2019-07-12 18:38:28 +0800

    vue

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,6 @@ import store from './store';
 import axios from 'axios';
 import VueAxios from 'vue-axios';
 Vue.prototype.$axios = axios
-// import ajax from './js/custom';
-// Vue.use(ajax);
 
 Vue.config.productionTip = false;
 
